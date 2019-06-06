5a674493875fb407054885d88a93812b8f3d1c7d
Punnoose <punnoosekuttyj@yahoo.com>
2019-06-06 05:56:46 -0400

    ps training

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { FieldsComponent } from './fields/fields.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { FieldComponent } from './field/field.component';
+import { UniqueDirective } from './unique.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FieldsComponent,
+    DynamicFormComponent,
+    FieldComponent,
+    UniqueDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
